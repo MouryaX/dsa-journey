a9040acfeb897c00922cd7c11adeebde8b3a1b1e
Mourya5AL <mouryagoguri4@gmail.com>
2026-06-04 23:01:49 +0530

    04-06-2025

--- 06.Recursion/06.Reverse an array.py
+++ 06.Recursion/06.Reverse an array.py
@@ -4,10 +4,10 @@ def rev(l,r,arr):
         return 
     arr[l],arr[r]=arr[r],arr[l]
     rev(l+1,r-1,arr)
-    return arr
-
+    #return arr no need to return the arr which is already modified in-place
 arr=[8,7,6,5,4,3,2,1]
 print(rev(0,len(arr)-1,arr))
+print(arr)
 
 #Functional recursion using single index
 def rev(arr,i,n):
@@ -15,9 +15,10 @@ def rev(arr,i,n):
         return
     arr[i],arr[n-i-1]=arr[n-i-1],arr[i]
     rev(arr,i+1,n)
-    return
 
 arr=[8,7,6,5,4,3,2,1]
 print(rev(arr,0,len(arr)))
+print(arr)
 
-#Time:O(n) for both Space:O(n) stack space 
+#Time:O(n//2) = O(n) for both approaches
+#Space:O(n//2) = O(n) stack space 
