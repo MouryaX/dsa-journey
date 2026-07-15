9312c6bc1f8b564ca2181a5b141d2e1a73d0e2c3
Mourya5AL <mouryagoguri4@gmail.com>
2026-07-15 17:19:33 +0530

    15-07-2026

--- test.py
+++ test.py
@@ -1,54 +1,13 @@
-class Node:
-    def __init__(self,data):
-        self.data = data
-        self.next = None
-
-class LinkedList:
-    def __init__(self):
-        self.head=None
-    
-    def insert(self,data):
-        new_node = Node(data)
-        
-        if self.head is None:
-            self.head=new_node
-            return
-        
-        temp = self.head
-        while temp.next:
-            temp=temp.next
-            
-        temp.next = new_node
-    def display(self):
-        
-        temp = self.head
-        
-        while temp:
-            print(temp.data, end="->")
-            temp = temp.next
-        
-        print("None")
-        
-    def rev(self):
-        left=2
-        right=4
-        temp=self.head
-        c=0
-        while temp:
-            if c+1 == left:
-                first=temp
-                start = temp.next
-            if c == right:
-                end = temp
-                last=temp.next
+# cook your dish here
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    arr=list(map(int,input().split()))
+    ans=arr[0]>>1
+    print(ans)
+    c=1
+    for i in range(1,n):
+        while arr[i]> ans:
+            arr[i]=arr[i]>>1
             c+=1
-            temp=temp.next
-        return start,end,first,last
-            
-ll=LinkedList()
-for i in range(1,6):
-    ll.insert(i)
-l,r,f,la=ll.rev()
-print(l.data,r.data,f.data,la.data)
-ll.display()
-     
+    print(c)
